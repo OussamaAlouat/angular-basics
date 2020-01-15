cc7557da81ba3d6c651a93bb079de80ccf31e2fa
oussamaAlouat <al317257@uji.es>
2020-01-15 15:37:29 +0100

    Click and interpolation

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.scss']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+  public allowNewServer: boolean;
+  public serverCreationStatus: string;
+  constructor() {
+    this.allowNewServer = true;
+    this.serverCreationStatus = 'No server was created!';
+  }
 
   ngOnInit() {
   }
 
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created';
+  }
 }
