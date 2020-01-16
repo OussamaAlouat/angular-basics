f94d3438fc7c64170722dcf3e5a70f8f887f0d38
oussamaAlouat <al317257@uji.es>
2020-01-16 09:40:35 +0100

    Styling on angular

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,7 +1,13 @@
 import {Component} from '@angular/core';
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styles: [`
+    .online {
+      color: white;
+    }
+    `
+  ]
 })
 
 export class ServerComponent {
@@ -9,7 +15,11 @@ export class ServerComponent {
   public serverStatus:string;
 
   constructor() {
-    this.serverStatus = 'offline';
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
     this.serverId = 10;
+  }
+
+  getColor() {
+    return this.serverStatus === 'online'? 'green' : 'red';
   }
 }
